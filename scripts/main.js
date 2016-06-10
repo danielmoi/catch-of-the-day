06cbc8039344873a1ed7e3279aa2e85bbee34008
danielmoi <daniel@ampersandmoi.com>
2016-06-10 13:55:00 +1000

    Create first component, StorePicker

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+// StorePicker
+// This will let us create a <StorePicker /> element
+
+var StorePicker = React.createClass({
+  render: function() {
+    return (
+      <p>STORE PICKER</p>
+    )
+  }
+});
