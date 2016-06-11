c03f248d6ad6caa7faa9c90bdf63198e69cd746b
danielmoi <daniel@ampersandmoi.com>
2016-06-11 17:06:40 +1000

    Create AddFishForm and retrieve values

--- scripts/main.js
+++ scripts/main.js
@@ -27,6 +27,41 @@ var App = React.createClass({
   }
 })
 
+
+// Add Fish Form
+var AddFishForm = React.createClass({
+  createFish: function(e) {
+    e.preventDefault();
+
+    // Take data from form and create an object
+    var fish = {
+      name: this.refs.name.value,
+      price: this.refs.price.value,
+      status: this.refs.status.value,
+      desc: this.refs.desc.value,
+      image: this.refs.image.value
+    };
+
+    console.log(fish);
+    // Add fish to App State
+  },
+  render: function() {
+    return (
+      <form className="fish-edit" onSubmit={ this.createFish }>
+        <input type="text" ref="name" placeholder="Fish Name"/>
+        <input type="text" ref="price" placeholder="Fish Price"/>
+        <select ref="status">
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <textarea type="text" ref="desc" placeholder="Desc"></textarea>
+        <input type="text" ref="image" placeholder="URL to image"/>
+        <button type="submit">Add Item</button>
+      </form>
+    )
+  }
+})
+
 // Header
 var Header = React.createClass({
   render: function() {
@@ -59,7 +94,10 @@ var Order = React.createClass({
 var Inventory = React.createClass({
   render: function() {
     return (
-      <p>INVENTORY</p>
+      <div>
+        <h2>INVENTORY</h2>
+        <AddFishForm />
+      </div>
     )
   }
 });
