db6e0307661681c97e2f324f71c2cf609efe23c1
danielmoi <daniel@ampersandmoi.com>
2016-06-11 11:46:18 +1000

    Transition from StorePicker to App Use History mixin, onSubmit event handler, and refs

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 
+var History = ReactRouter.History;
+
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers.js');
@@ -65,9 +67,19 @@ var Inventory = React.createClass({
 // StorePicker
 // This will let us create a <StorePicker /> element
 var StorePicker = React.createClass({
+  mixins: [ History ],
+  goToStore: function(e) {
+    e.preventDefault();
+
+    // get data from input
+    var storeId = this.refs.storeId.value;
+
+    // transition from StorePicker to App
+    this.history.pushState(null, '/store/' + storeId);
+  },
   render: function() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ this.goToStore }>
         <h2>Please Enter a Store</h2>
         <input
           type="text"
