81aaefe84593491363ed527cfa3fd167eeb4ebc7
danielmoi <daniel@ampersandmoi.com>
2016-06-14 21:55:09 +1000

    Render details of order

--- scripts/main.js
+++ scripts/main.js
@@ -157,6 +157,25 @@ var Header = React.createClass({
 
 // Order
 var Order = React.createClass({
+  renderOrder: function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if (!fish) {
+      return (
+        <li key={ key }>Sorry, fish no longer available!</li>
+      )
+    }
+
+    return (
+
+      <li>
+        { count }lbs
+         { fish.name }
+        <span className="price">{ h.formatPrice(count * fish.price) }</span>
+      </li>
+    )
+  },
   render: function() {
     var orderIDs = Object.keys(this.props.order);
     var total = orderIDs.reduce((total, next) => {
@@ -174,6 +193,7 @@ var Order = React.createClass({
       <div className="order-wrap">
         <h2 className="order-title">Your Order</h2>
         <ul className="order">
+          { orderIDs.map(this.renderOrder) }
           <li className="total">
             <strong>Total: </strong>{h.formatPrice(total)}</li>
         </ul>
