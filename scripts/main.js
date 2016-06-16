6725be8e00ab1bacdaf30a9b400f9e7cc1494523
danielmoi <daniel@ampersandmoi.com>
2016-06-16 21:43:45 +1000

    Use Local Storage to persist state

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,17 @@ var App = React.createClass({
       context: this,
       state: 'fishes'
     });
+    var localStorageRef = localStorage.getItem('order-' + this.props.params.storedId);
+
+    if (localStorageRef) {
+      // update our component state to reflect what is in localStorage
+      this.setstate({
+        order: JSON.parse(localStorageRef)
+      })
+    }
+  },
+  componentWillUpdate: function(nextProps, nextState) {
+    localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
   },
   _addFish: function(fish) {
     // create unique key for fish
@@ -178,7 +189,7 @@ var Order = React.createClass({
 
     return (
 
-      <li>
+      <li key={ key }>
         { count }lbs
          { fish.name }
         <span className="price">{ h.formatPrice(count * fish.price) }</span>
